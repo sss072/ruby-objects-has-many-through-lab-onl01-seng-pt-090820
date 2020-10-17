c68bd84ad7686ba475f56795c14921cf1acb0025
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-10-17 21:39:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,3 +9,18 @@ class Artist
   def self.all 
     @@all 
   end 
+  def new_song(name, genre)
+    song_holder = Song.new(name, self, genre)
+    song_holder.artist = self 
+  end 
+  def songs 
+    Song.all.select do |element|
+      element.artist == self 
+    end 
+  end 
+  def genres 
+    songs.collect do |element|
+      element.genre 
+    end 
+  end 
+end 
